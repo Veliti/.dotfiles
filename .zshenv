505d2a645f6ac91a96a0aeb1b1ec6730dc2ceb2d
Veliti <veliti@pop-os.localdomain>
2023-04-05 18:09:32 +0500

    addet linking script

---
+++ .zshenv
@@ -0,0 +1,5 @@
+export PATH=/home/veliti/.local/bin:$PATH
+export PATH=/home/veliti/.cargo/bin:$PATH
+export PATH=/home/veliti/.npm/bin:$PATH
+
+PROJ="$HOME/Documents/Projects"
---
+++ setup_links.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DOTFILES=$(pwd)
+
+# lvim link
+LVIM_PATH="$HOME/.config/lvim"
+ln -f $DOTFILES/config.lua $LVIM_PATH/config.lua
+
+# zsh links
+ln -sf $DOTFILES/.zshrc $HOME/.zshrc
+ln -sf $DOTFILES/.zshenv $HOME/.zshenv
