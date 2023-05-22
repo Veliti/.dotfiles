fd1783d005c509bda5711f18b496eda6c4038c08
Veliti <71709424+Veliti@users.noreply.github.com>
2023-05-22 19:41:58 +0500

    donno2

--- lazyvim/.config/nvim/init.lua
+++ lazyvim/.config/nvim/init.lua
@@ -1,4 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.opt.relativenumber = false
+local cap = vim.lsp.protocol.make_client_capabilities()
+cap.textDocument.completion.completionItem.snippetSupport = true
+
+require("lspconfig").html.setup({
+  capabilities = cap,
+})
